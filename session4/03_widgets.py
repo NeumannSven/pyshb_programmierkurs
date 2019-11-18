c7fd358f226209f77efc55542cbea48667c597ae
Thomas Helmke <thomas.helmke@gmx.de>
2019-11-18 10:49:26 +0100

    erster Teil der Widgetübersicht

--- session4/03_widgets.py
+++ session4/03_widgets.py
@@ -1,20 +1,31 @@
 from tkinter import *
 
+# Hauptfenster erzeugen
 root = Tk()
 root.geometry('500x500')
 
+# Label erzeugen
 abschnitt1 = Label(root, text="Anzeige von Informationen")
 
 abschnitt1.grid(row=0, column=0, sticky=W+E)
 
+# Message vorbereiten und anzeigen
 zen = "Beautiful is better than ugly.\n" \
 "Explicit is better than implicit.\n" \
 "Simple is better than complex.\n" \
 "Complex is better than complicated."
 
-nachricht1 = Message(root, text=zen)
+nachricht1 = Message(root, text=zen) #, width=500
 nachricht1.config(bg='lightgreen', font=('times', 24, 'italic'))
 
 nachricht1.grid(row=1, column=0, sticky=W+E)
+root.grid_columnconfigure(0, weight=1)
+
+# Graphik erzeugen
+zeichnung1 = Canvas(root, width=200, height=100)
+
+zeichnung1.grid(row=2, column=0, sticky=W+E)
+zeichnung1.create_line(0, 0, 200, 100)
+zeichnung1.create_rectangle(50, 25, 150, 75, fill="green")
 
 root.mainloop()
