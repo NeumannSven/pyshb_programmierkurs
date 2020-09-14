a4dd4f977c7fdc6a119681d4e1e20c7afad2656c
bachmann-m200 <sven.neumann@bachmann.info>
2020-09-14 14:02:56 +0200

    create file demolib.py

---
+++ session11/demolib.py
@@ -0,0 +1,60 @@
+'''
+Created on 01.09.2020
+
+@author: neumann
+'''
+import random
+
+class ValueList:
+    """create a list with size of count and with random
+    values between minvalue and maxvalue
+
+    Args:
+        count (int): size of list
+        minvalue (int, optional): min random value. Defaults to 0.
+        maxvalue (int, optional): max random value. Defaults to 1000.
+
+    """   
+    def __init__(self, count, minvalue=0, maxvalue=1000): 
+        self._values = [random.randrange(minvalue, maxvalue+1) for i in range(count)]
+
+    def getValues(self):
+        """return the list of values
+
+        Returns:
+            list: list of values
+        """        
+        return self._values.copy()
+
+    def getMaxValue(self):
+        """return the biggest value from the list 
+
+        Returns:
+            int: the biggest value in the list
+        """        
+        return max(self._values)
+
+    def getMinValue(self):
+        """return the smallest value from the list
+
+        Returns:
+            int: the smallest value in the list
+        """        
+        return min(self._values)
+
+    def getSum(self):
+        """return the sum over all values in the list
+
+        Returns:
+            int: sum over all values
+        """        
+        result = 0
+        for value in self._values:
+            result += value
+        return result
+
+
+if __name__ == "__main__":
+    mylist = ValueList(15, 0, 2)
+    print(mylist.getValues())
+    
