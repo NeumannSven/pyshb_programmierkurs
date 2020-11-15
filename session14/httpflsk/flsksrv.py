a11865b6833632e2b6fff4b033e9efe3583655cc
Sven Neumann <seven.newman@icloud.com>
2020-11-15 18:29:27 +0100

    Add files via upload

---
+++ session14/httpflsk/flsksrv.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+app.run()
