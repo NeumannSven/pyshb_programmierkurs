8d11c9bb94cf6260c543fd9c1649f4cdef72515b
Sven Neumann <sven.neumann@web.de>
2020-12-01 21:05:28 +0100

    word doc parsen

--- session15/convert.py
+++ session15/convert.py
@@ -1,7 +1,36 @@
 
-
+from pprint import pprint
 from docx import Document
 
-doc = Document("session15/Tageshoroskope 2020/Januar 2020/01 Mi. 01.01.20_.docx")
+#doc = Document("session15/Tageshoroskope 2020/April 2020/01 Mi, 01.04.20.docx")
 
-print(dir(doc))
+doc = Document("session15/Tageshoroskope 2020/Januar 2020/01 Mi. 01.01.20_.docx")
+horolist = {"Widder": "", "Stier": "", "Zwilling": "", "Krebs": "", "Löwe": "", "Jungfrau": "", "Waage": "", "Skorpion": "", "Schütze": "", "Steinbock": "", "Wassermann": "", "Fische": ""}
+
+start = False
+text = ""
+
+for paragraph in doc.paragraphs:
+    weiter = False
+
+    for tierkreiszeichen in horolist.keys():
+        if paragraph.text.startswith(tierkreiszeichen) and start:
+            start = False
+            horolist[actualZodiac] = text
+            text = ""
+        
+        if paragraph.text.startswith(tierkreiszeichen) and not start:
+            actualZodiac = tierkreiszeichen
+            start = True
+            weiter = True
+            break
+
+    if weiter:
+        continue
+
+    if start:
+        text += paragraph.text
+
+horolist[actualZodiac]=text
+pprint(horolist)
+    
