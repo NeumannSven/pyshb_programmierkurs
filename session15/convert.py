58df459c85b553ddf795b923edcd7a96516f8a3e
Sven Neumann <sven.neumann@web.de>
2020-12-01 21:39:26 +0100

    create csv files

--- session15/convert.py
+++ session15/convert.py
@@ -2,35 +2,43 @@
 from pprint import pprint
 from docx import Document
 
-#doc = Document("session15/Tageshoroskope 2020/April 2020/01 Mi, 01.04.20.docx")
+doclist = ("session15/Tageshoroskope 2020/April 2020/01 Mi, 01.04.20.docx", "session15/Tageshoroskope 2020/Januar 2020/01 Mi. 01.01.20_.docx")
+yearlist =[]
 
-doc = Document("session15/Tageshoroskope 2020/Januar 2020/01 Mi. 01.01.20_.docx")
-horolist = {"Widder": "", "Stier": "", "Zwilling": "", "Krebs": "", "Löwe": "", "Jungfrau": "", "Waage": "", "Skorpion": "", "Schütze": "", "Steinbock": "", "Wassermann": "", "Fische": ""}
+for wordfile in doclist:
+    horolist = {"Widder": "", "Stier": "", "Zwilling": "", "Krebs": "", "Löwe": "", "Jungfrau": "", "Waage": "", "Skorpion": "", "Schütze": "", "Steinbock": "", "Wassermann": "", "Fische": ""}
+    doc = Document(wordfile)
+    start = False
+    text = ""
 
-start = False
-text = ""
+    for paragraph in doc.paragraphs:
+        weiter = False
 
-for paragraph in doc.paragraphs:
-    weiter = False
+        for tierkreiszeichen in horolist.keys():
+            if paragraph.text.startswith(tierkreiszeichen) and start:
+                start = False
+                horolist[actualZodiac] = text.strip()
+                text = ""
+            
+            if paragraph.text.startswith(tierkreiszeichen) and not start:
+                actualZodiac = tierkreiszeichen
+                start = True
+                weiter = True
+                break
 
-    for tierkreiszeichen in horolist.keys():
-        if paragraph.text.startswith(tierkreiszeichen) and start:
-            start = False
-            horolist[actualZodiac] = text
-            text = ""
+        if weiter:
+            continue
+
+        if start:
+            text += paragraph.text + " "
+            text.replace("  ", " ")
+
+    horolist[actualZodiac]=text.strip()
+    yearlist.append(horolist)
+
+for tierkreiszeichen in horolist.keys():
+    f = open(tierkreiszeichen + ".csv", "w")
+    for day in yearlist:
+        f.write(f"Datum; {day[tierkreiszeichen]}\n")
+    f.close()
         
-        if paragraph.text.startswith(tierkreiszeichen) and not start:
-            actualZodiac = tierkreiszeichen
-            start = True
-            weiter = True
-            break
-
-    if weiter:
-        continue
-
-    if start:
-        text += paragraph.text
-
-horolist[actualZodiac]=text
-pprint(horolist)
-    
